37f94611ff0bc3a6b2a540f963bbe29779bad34d
dryomina-v <feof90719@gmail.com>
2026-03-23 19:15:20 +0300

    Added balance calculation for transactions and selection of transactions by category

--- database/Database.h
+++ database/Database.h
@@ -1,28 +1,29 @@
 #pragma once
-#include <vector>
 #include "../models/Transaction.h"
 #include "../models/User.h"
 #include <sqlite3.h>
+#include <vector>
 
 class Database {
 public:
-    Database(const std::string& db_name);
-    ~Database();
+  Database(const std::string &db_name);
+  ~Database();
 
-    void init();
+  void init();
 
-    void addTransaction(const Transaction& t);
-    std::vector<Transaction> getTransactionsByUser(int user_id);
+  void addTransaction(const Transaction &t);
+  std::vector<Transaction> getTransactionsByUser(int user_id);
 
-    void clearTransactions();
+  void clearTransactions();
 
-    bool addUser(const std::string& login, const std::string& password);
-    User getUserByLogin(const std::string& login);
+  bool addUser(const std::string &login, const std::string &password);
+  User getUserByLogin(const std::string &login);
 
-    double getBalanceByUser(int user_id);
+  double getBalanceByUser(int user_id);
 
-    std::vector<Transaction> getTransactionsByCategory(int user_id, const std::string& category);
+  std::vector<Transaction>
+  getTransactionsByCategory(int user_id, const std::string &category);
 
 private:
-    sqlite3* db = nullptr;
+  sqlite3 *db = nullptr;
 };
