93d6d0fee46b30d4f185cd11928df0fd807026f5
dryomina-v <feof90719@gmail.com>
2026-03-23 19:19:35 +0300

    Added new actions for getting balance and working with categories

--- server/Session.cpp
+++ server/Session.cpp
@@ -1,93 +1,67 @@
 #include "Session.h"
 #include <iostream>
 
-Session::Session(tcp::socket socket,
-                 UserHandler& userHandler,
-                 TransactionHandler& transactionHandler)
-    : socket_(std::move(socket)),
-      userHandler_(userHandler),
-      transactionHandler_(transactionHandler)
-{}
+Session::Session(tcp::socket socket, UserHandler &userHandler,
+                 TransactionHandler &transactionHandler)
+    : socket_(std::move(socket)), userHandler_(userHandler),
+      transactionHandler_(transactionHandler) {}
 
-void Session::start() {
-    read();
-}
+void Session::start() { read(); }
 
 void Session::read() {
-    auto self = shared_from_this();
+  auto self = shared_from_this();
 
-    boost::asio::async_read_until(
-        socket_,
-        buffer_,
-        "\n",
-        [this, self](boost::system::error_code ec, std::size_t) {
-            if (!ec) {
-                std::istream is(&buffer_);
-                std::string line;
-                std::getline(is, line);
+  boost::asio::async_read_until(
+      socket_, buffer_, "\n",
+      [this, self](boost::system::error_code ec, std::size_t) {
+        if (!ec) {
+          std::istream is(&buffer_);
+          std::string line;
+          std::getline(is, line);
 
-                process(line);
-            }
-        });
+          process(line);
+        }
+      });
 }
 
-void Session::process(const std::string& line) {
-    json response;
+void Session::process(const std::string &line) {
+  json response;
 
-    try {
-        auto j = json::parse(line);
+  try {
+    auto j = json::parse(line);
 
-        if (!j.contains("action")) {
-            response = { {"status","error"}, {"message","Missing action"} };
-        }
-        else if (j["action"] == "register") {
-            response = userHandler_.registerUser(
-                j["login"],
-                j["password"]
-            );
-        }
-        else if (j["action"] == "login") {
-            response = userHandler_.loginUser(
-                j["login"],
-                j["password"]
-            );
-        }
-        else if (j["action"] == "add_transaction") {
-            response = transactionHandler_.addTransaction(
-                j["user_id"],
-                j["type"],
-                j["amount"]
-            );
-        }
-        else if (j["action"] == "get_transactions") {
-            response = transactionHandler_.getTransactions(
-                j["user_id"]
-            );
-        }
-        else {
-            response = {
-                {"status","error"},
-                {"message","Unknown action"}
-            };
-        }
-
-    } catch (json::parse_error&) {
-        response = {
-            {"status","error"},
-            {"message","Invalid JSON"}
-        };
+    if (!j.contains("action")) {
+      response = {{"status", "error"}, {"message", "Missing action"}};
+    } else if (j["action"] == "register") {
+      response = userHandler_.registerUser(j["login"], j["password"]);
+    } else if (j["action"] == "login") {
+      response = userHandler_.loginUser(j["login"], j["password"]);
+    } else if (j["action"] == "add_transaction") {
+      std::string category = j.contains("category") ? j["category"] : "";
+      response = transactionHandler_.addTransaction(j["user_id"], j["type"],
+                                                    j["amount"], category);
+    } else if (j["action"] == "get_transactions") {
+      response = transactionHandler_.getTransactions(j["user_id"]);
+    } else if (j["action"] == "get_balance") {
+      response = transactionHandler_.getBalance(j["user_id"]);
+    } else if (j["action"] == "get_transactions_by_category") {
+      response = transactionHandler_.getTransactionsByCategory(j["user_id"],
+                                                               j["category"]);
+    } else {
+      response = {{"status", "error"}, {"message", "Unknown action"}};
     }
 
-    write(response.dump() + "\n");
+  } catch (json::parse_error &) {
+    response = {{"status", "error"}, {"message", "Invalid JSON"}};
+  }
+
+  write(response.dump() + "\n");
 }
 
-void Session::write(const std::string& response) {
-    auto self = shared_from_this();
+void Session::write(const std::string &response) {
+  auto self = shared_from_this();
 
-    boost::asio::async_write(
-        socket_,
-        boost::asio::buffer(response),
-        [this, self](boost::system::error_code ec, std::size_t) {
-            socket_.close();
-        });
+  boost::asio::async_write(socket_, boost::asio::buffer(response),
+                           [this, self](boost::system::error_code ec,
+                                        std::size_t) { socket_.close(); });
 }
