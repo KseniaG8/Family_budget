ad6863916566ad6161e5676751137574582ecc05
qwxsthal <pykhalin@edu.hse.ru>
2026-05-06 22:48:30 +0300

    Update Database.h

--- database/Database.h
+++ database/Database.h
@@ -1,29 +1,48 @@
 #pragma once
 #include "../models/Transaction.h"
 #include "../models/User.h"
+#include "../models/Goal.h"
 #include <sqlite3.h>
 #include <vector>
 
 class Database {
 public:
-  Database(const std::string &db_name);
-  ~Database();
+    bool enable2FA(int user_id, const std::string &secret);
+    Database(const std::string &db_name);
+    ~Database();
 
-  void init();
+    void init();
 
-  void addTransaction(const Transaction &t);
-  std::vector<Transaction> getTransactionsByUser(int user_id);
+    void addTransaction(const Transaction &t);
+    std::vector<Transaction> getTransactionsByUser(int user_id);
 
-  void clearTransactions();
+    void clearTransactions();
 
-  bool addUser(const std::string &login, const std::string &password);
-  User getUserByLogin(const std::string &login);
+    bool addUser(const std::string &login, const std::string &password);
+    User getUserByLogin(const std::string &login);
 
-  double getBalanceByUser(int user_id);
+    bool updateTransaction(int transaction_id, const std::string &type, double amount, const std::string &category);
 
-  std::vector<Transaction>
-  getTransactionsByCategory(int user_id, const std::string &category);
+    double getBalanceByUser(int user_id);
+
+    std::vector<Transaction> getTransactionsByCategory(int user_id, const std::string &category);
+
+    bool deleteTransaction(int transaction_id);
+
+    Transaction getTransactionById(int transaction_id);
+
+    bool setLimit(int user_id, const std::string &category, double limit_amount);
+    double getLimit(int user_id, const std::string &category);
+    double getSpentByCategory(int user_id, const std::string &category);
+
+    int createFamily(const std::string &family_name);
+    bool joinFamily(int user_id, int family_id);
+    int getUserFamilyId(int user_id);
+
+    bool addGoal(int family_id, const std::string &name, double target_amount);
+    std::vector<Goal> getGoalsByFamily(int family_id);
+    bool addMoneyToGoal(int goal_id, double amount);
 
 private:
-  sqlite3 *db = nullptr;
+    sqlite3 *db = nullptr;
 };
