399352e25b50daf48903381b663e04d7a6bcd10a
dryomina-v <feof90719@gmail.com>
2026-05-06 15:54:22 +0300

    Added deletion, limitation, and retrieval of transactions by ID

--- database/Database.h
+++ database/Database.h
@@ -6,24 +6,33 @@
 
 class Database {
 public:
-  Database(const std::string &db_name);
-  ~Database();
+    Database(const std::string &db_name);
+    ~Database();
 
-  void init();
+    void init();
 
-  void addTransaction(const Transaction &t);
-  std::vector<Transaction> getTransactionsByUser(int user_id);
+    void addTransaction(const Transaction &t);
+    std::vector<Transaction> getTransactionsByUser(int user_id);
 
-  void clearTransactions();
+    void clearTransactions();
 
-  bool addUser(const std::string &login, const std::string &password);
-  User getUserByLogin(const std::string &login);
+    bool addUser(const std::string &login, const std::string &password);
+    User getUserByLogin(const std::string &login);
 
-  double getBalanceByUser(int user_id);
+    bool updateTransaction(int transaction_id, const std::string &type, double amount, const std::string &category);
 
-  std::vector<Transaction>
-  getTransactionsByCategory(int user_id, const std::string &category);
+    double getBalanceByUser(int user_id);
+
+    std::vector<Transaction> getTransactionsByCategory(int user_id, const std::string &category);
+
+    bool deleteTransaction(int transaction_id);
+
+    Transaction getTransactionById(int transaction_id);
+
+    bool setLimit(int user_id, const std::string &category, double limit_amount);
+    double getLimit(int user_id, const std::string &category);
+    double getSpentByCategory(int user_id, const std::string &category);
 
 private:
-  sqlite3 *db = nullptr;
+    sqlite3 *db = nullptr;
 };
