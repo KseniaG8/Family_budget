145d3da426d7c60141a34c679f7b7860bb0be5ff
qwxsthal <pykhalin@edu.hse.ru>
2026-05-17 23:57:03 +0300

    Update Transaction.h

--- models/Transaction.h
+++ models/Transaction.h
@@ -4,7 +4,10 @@
 struct Transaction {
   int id;
   int user_id;
+  int group_id = -1;
   std::string type;
   double amount;
   std::string category;
+  std::string currency;    
+  std::string description; 
 };
