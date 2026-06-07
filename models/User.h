20ab477a0a723ba2f0e7264ada9b55cf62d1b610
dryomina-v <feof90719@gmail.com>
2026-06-07 17:33:14 +0300

    Update User.h

--- models/User.h
+++ models/User.h
@@ -6,6 +6,5 @@ struct User {
     std::string login;
     std::string password;
     std::string totp_secret; 
-    int is_2fa_enabled;      
-    int family_id;           
+    int is_2fa_enabled;              
 };
