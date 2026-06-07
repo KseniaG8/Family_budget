adbb278ba583155d32ec180bf8a465be7dec687e
dryomina-v <feof90719@gmail.com>
2026-06-07 16:30:37 +0300

    Implementation of adding a participant by login

--- services/UserService.h
+++ services/UserService.h
@@ -1,18 +1,23 @@
 #pragma once
-#include "../database/Database.h"
-#include <string>
 #include <nlohmann/json.hpp>
+#include <string>
+#include "../database/Database.h"
+#include "../models/User.h"
 
 class UserService {
 public:
-    UserService(Database& db);
+    UserService(Database &db);
 
-    std::string registerUser(const std::string& login, const std::string& password);
-    std::string loginUser(const std::string& login, const std::string& password);
-    
-    nlohmann::json setup2FA(int user_id, const std::string& login);
-    bool verifyLogin2FA(const std::string& login, const std::string& code);
+    std::string
+    registerUser(const std::string &login, const std::string &password);
+    nlohmann::json
+    loginUser(const std::string &login, const std::string &password);
+    User getUserByLogin(const std::string &login);
+
+    nlohmann::json setup2FA(int user_id, const std::string &login);
+    nlohmann::json
+    verifyLogin2FA(const std::string &login, const std::string &code);
 
 private:
-    Database& database;
+    Database &database;
 };
