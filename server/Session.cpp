3440e54d708f02a51d6054b27c58a7545df089ff
qwxsthal <pykhalin@edu.hse.ru>
2026-06-13 16:06:08 +0300

    Update Session.cpp

--- server/Session.cpp
+++ server/Session.cpp
@@ -139,6 +139,16 @@ void Session::handle_request() {
             return;
         }
 
+        if (request_.method() == http::verb::get &&
+            target.find("/analytics/categories?") == 0) {
+            int user_id = std::stoi(get_query_param(target, "user_id"));
+
+            auto result = transactionHandler_.getCategoryStatistics(user_id);
+
+            send_response(result);
+            return;
+        }
+
         if (request_.method() == http::verb::post && target == "/limits") {
             json body = json::parse(request_.body());
 
@@ -196,7 +206,7 @@ void Session::handle_request() {
             send_response(result);
             return;
         }
-        
+
         if (request_.method() == http::verb::put && target == "/goals") {
             json body = json::parse(request_.body());
 
@@ -242,13 +252,18 @@ void Session::handle_request() {
             return;
         }
 
-        if (request_.method() == http::verb::post && target == "/groups/members") {
+        if (request_.method() == http::verb::post &&
+            target == "/groups/members") {
             json body = json::parse(request_.body());
-            std::string login = body["login"].get<std::string>(); 
-            send_response(transactionHandler_.addUserToGroup(body["group_id"], login));
+
+            auto result = transactionHandler_.addUserToGroup(
+                body["group_id"], body["login"]
+            );
+
+            send_response(result);
             return;
         }
-        
+
         if (request_.method() == http::verb::delete_ &&
             target.find("/groups/members?") == 0) {
             int group_id = std::stoi(get_query_param(target, "group_id"));
@@ -335,20 +350,16 @@ void Session::handle_request() {
 
         if (request_.method() == http::verb::post && target == "/2fa/setup") {
             json body = json::parse(request_.body());
-            send_response(userHandler_.setup2FA(body["user_id"], body["login"]));
+            send_response(userHandler_.setup2FA(body["user_id"], body["login"])
+            );
             return;
         }
 
         if (request_.method() == http::verb::post && target == "/2fa/verify") {
             json body = json::parse(request_.body());
-            send_response(userHandler_.verifyLogin2FA(body["login"], body["code"]));
-            return;
-        }
-
-        // --- Аналитика ---
-        if (request_.method() == http::verb::get && target.find("/analytics/categories?") == 0) {
-            int user_id = std::stoi(get_query_param(target, "user_id"));
-            send_response(transactionHandler_.getCategoryAnalytics(user_id));
+            send_response(
+                userHandler_.verifyLogin2FA(body["login"], body["code"])
+            );
             return;
         }
 
